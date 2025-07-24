3736025294d627b10c53e9377790aea2f0b2f641
Hung998S <Hungpd998@gmail.com>
2025-07-24 23:49:08 +0700

    Display HTML

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Education For Nation")
+    return render(request,'home.html')
