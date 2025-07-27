01517ff96a5736a3ea32fade3e5a6f48b44753fd
Hung998S <Hungpd998@gmail.com>
2025-07-27 21:15:22 +0700

    show feacherd post

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from blogs.models import Category
+from blogs.models import Category, Blogs
 
 def home(request):
     categories = Category.objects.all()
+    featrued_post = Blogs.objects.filter(is_feacherd = True )
+    # print(featrued_post)
     context = {
-        'categories':categories
+        'categories':categories,
+        'featured_post': featrued_post
     }
     return render(request,'home.html', context)
