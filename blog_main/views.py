03653bf2b6757c51c65e9bb91d479bcd70e1db4e
Hung998S <Hungpd998@gmail.com>
2025-07-28 12:48:59 +0700

    Show Simple Post

--- blog_main/views.py
+++ blog_main/views.py
@@ -3,10 +3,12 @@ from blogs.models import Category, Blogs
 
 def home(request):
     categories = Category.objects.all()
-    featrued_post = Blogs.objects.filter(is_feacherd = True )
-    # print(featrued_post)
+    featrued_post = Blogs.objects.filter(is_feacherd = True, status = 'published' )
+    posts = Blogs.objects.filter(is_feacherd = False, status='published')
+    
     context = {
         'categories':categories,
-        'featured_post': featrued_post
+        'featured_post': featrued_post,
+        'posts': posts
     }
     return render(request,'home.html', context)
