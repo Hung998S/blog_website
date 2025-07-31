d8fb40ae5af890d3d1f65d646c1634c0c471531a
Hung998S <Hungpd998@gmail.com>
2025-07-31 12:15:56 +0700

    Category counts posts counts

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from blogs.models import Category, Blogs
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    category_counts = Category.objects.all().count()
+    blogs_counts = Blogs.objects.all().count()
+    context = {
+        'category_counts' : category_counts,
+        'blogs_counts': blogs_counts
+    }
+
+    return render(request, 'dashboard/dashboard.html', context)
